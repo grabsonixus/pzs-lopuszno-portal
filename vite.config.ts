534db0ebbdfcad3b70ddb49d08979f46aa2c9f9d
Grabson <krzysztofxgrabka@gmail.com>
2026-03-26 17:43:44 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: "dist",
       assetsDir: "assets",
-      chunkSizeWarningLimit: 700,
+      chunkSizeWarningLimit: 1000,
 
       rollupOptions: {
         output: {
